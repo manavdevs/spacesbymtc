c1c57b3a77bfcd03908b76fe900514ebf9e8342c
SidPyc <siddhufs2003@gmail.com>
2024-09-19 17:48:05 +0530

    photo rename3

--- app/components/ExpandableCardDemo.tsx
+++ app/components/ExpandableCardDemo.tsx
@@ -4,7 +4,7 @@ const ExpandableCardDemo = () => {
       id: 1,
       name: 'Syed Abdul Wahab',
       designation: 'Chairman and Founder',
-      image: '/images/peoples/2.jpg',
+      image: '/images/peoples/syedabdulwahab.jpg',
       companies: [
         { name: 'MINDSTEC', logo: '/images/companies/mindstec.png' },
         { name: 'MIANTIC AV', logo: '/images/companies/miantic.png' },
