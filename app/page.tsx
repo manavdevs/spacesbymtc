efe3c3721cec0d3fc9db4dfe400a6b8534882c1e
ᗰᗩᑎᗩᐯ Kᗩᖇᗯᗩ <manavkarwa2016@gmail.com>
2024-11-08 18:12:39 +0530

    fin

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import Hero from "./components/Hero";
 import { SignupFormDemo } from "./components/contact";
 import VideoSplashScreen from "./components/VideoSplashScreen";
 import Offerings from "./components/Offerings";
-// import Whyus from "./components/Whyus";
+import Whyus from "./components/Whyus";
 import Testimonials from "./components/Testimonials";
 import Footer from "./components/Footer";
 import { InfiniteMovingCardsDemo } from "./components/InfiniteMovingCardsDemo";
@@ -24,21 +24,16 @@ const Page = () => {
       {isVideoPlaying ? (
         <VideoSplashScreen onVideoEnd={handleVideoEnd} />
       ) : (
-        // <div>
-        //   <Navbar />
-        //   <Hero />
-        //   <Offerings />
-        //   {/* <Whyus /> */}
-        //   <ImplementationMethodologies/>
-        //   <Testimonials />
-        //   <OurFactory/>
-        //   <SignupFormDemo /> 
-        //   <InfiniteMovingCardsDemo/>
-        //   <Footer />
-
-        // </div>
         <div>
-          <h1 className="flex justify-center items-center">Temporarily Out of Service</h1>
+        <Navbar />
+        <Hero />
+        <Offerings />
+        <ImplementationMethodologies/>
+        <Testimonials />
+        <OurFactory/>
+        <SignupFormDemo /> 
+        <InfiniteMovingCardsDemo/>
+        <Footer />
         </div>
       )}
     </>
